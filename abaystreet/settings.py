f85a93c95e00792101f203d544a42e8d9ad12ced
Mukhagali Aryn <mukhagaliaryn@gmail.com>
2021-08-06 13:25:31 +0600

    Deploying commit

--- abaystreet/settings.py
+++ abaystreet/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'abaystreet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT')
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT')
     }
 }
 # ===============================================================
